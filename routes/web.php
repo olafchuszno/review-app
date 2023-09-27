0e7883b5e7e1c4f1772825b257c0d868692f0f81
Olaf <olaf.chuszno@gmail.com>
2023-09-27 21:21:42 +0200

    Create airport search form on homepage

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::post('/airports', function () {
+    dd('/airports');
+    return view('index');
+});
