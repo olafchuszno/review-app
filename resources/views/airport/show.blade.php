365fbe4b1736d2bfdcf96170ad0556ce85a915fe
Olaf <olaf.chuszno@gmail.com>
2023-10-01 21:59:28 +0200

    Implement first section to airport.show view

--- resources/views/airport/show.blade.php
+++ resources/views/airport/show.blade.php
@@ -1,8 +1,42 @@
 @props(['airport'])
 <x-layout>
-    <div class="flex justify-center">
-        <h1 class="text-3xl color-red">
-            {{ $airport->name }} Page
-        </h1>
+
+<main class="flex justify-center">
+
+    <div class="w-2/3 gap-3 flex flex-col items-center">
+
+        <div class="px-4 sm:px-0">
+            <h3 class="text-base font-semibold leading-7 text-gray-900">{{ $airport->name }} Page</h3>
+            <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Basic information.</p>  </div>
+        <div class="mt-6 border-t border-gray-100">
+
+        <table class="divide-y divide-gray-100">
+
+            <x-basic_info>
+                <td class="text-sm py-2 font-medium leading-6 text-gray-900">Country name</td>
+                <td class="mt-1 text-sm py-2 leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $airport->countryName }}</td>
+            </x-basic_info>
+            <x-basic_info>
+                <td class="text-sm font-medium leading-6 text-gray-900">City name</td>
+                <td class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $airport->cityName }}</td>
+            </x-basic_info>
+            <x-basic_info>
+                <td class="text-sm font-medium leading-6 text-gray-900">Airport Code</td>
+                <td class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ $airport->code }}</td>
+            </x-basic_info>
+            <x-basic_info>
+                <td class="text-sm font-medium leading-6 text-gray-900">Location</td>
+                <td class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">It's located <strong>{{ $airport->city ? 'Inside' : 'Outside'}}</strong> the city borders </td>
+            </x-basic_info>
+
+            <x-basic_info>
+                <td class="text-sm font-medium leading-6 text-gray-900">About</td>
+                <td class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.</td>
+            </x-basic_info>
+        </dl>
     </div>
+    </div>
+
+</main>
+
 </x-layout>
---
+++ resources/views/components/basic_info.blade.php
@@ -0,0 +1,3 @@
+<tr class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 border-b-2 border-slate-200">
+    {{ $slot }}
+</tr>
