4a8e7bb3388386a0e273f132721494e46cfb96e3
Olaf <olaf.chuszno@gmail.com>
2023-10-02 17:05:44 +0200

    Move the seeder file to storage/app/public

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $path = '/Users/seiz/Documents/gist.githubusercontent.com_tanerdogan_10103011_raw_1b7cf2b5dfcb9be242e7cc8fbc1b95950b16becb_airports.sql.txt';
+        $path = 'storage/app/public/gist.githubusercontent.com_tanerdogan_10103011_raw_1b7cf2b5dfcb9be242e7cc8fbc1b95950b16becb_airports.sql.txt';
         DB::unprepared(file_get_contents($path));
         $this->command->info('Country table seeded!');
     }
