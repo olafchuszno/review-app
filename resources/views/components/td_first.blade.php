e01d39d7de18f69641fc103c2c77871cfbbd642b
Olaf <olaf.chuszno@gmail.com>
2023-10-02 12:53:06 +0200

    Delete blade components with old name

--- resources/views/components/td_first.blade.php
+++
@@ -1,3 +0,0 @@
-<td class="text-sm py-2 px-2 font-medium leading-6 text-gray-900">
-    {{ $slot }}
-</td>
--- resources/views/components/td_second.blade.php
+++
@@ -1,3 +0,0 @@
-<td class="mt-1 text-sm py-2 leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-    {{ $slot }}
-</td>
