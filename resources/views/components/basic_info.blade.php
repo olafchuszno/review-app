2fe4644ec6c8c22f879a38a5a5d834805a1613e7
Olaf <olaf.chuszno@gmail.com>
2023-10-02 10:52:37 +0200

    Allow sending custom classes to basic_info comp

--- resources/views/components/basic_info.blade.php
+++ resources/views/components/basic_info.blade.php
@@ -1,3 +1,4 @@
-<tr class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 border-b-2 border-slate-200">
+@props(['class' => ''])
+<tr class="{{ $class }} px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0 border-b-2 border-slate-200">
     {{ $slot }}
 </tr>
