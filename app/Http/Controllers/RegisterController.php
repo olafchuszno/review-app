0db172b227586e8d11d30e0d585f73cf9304db6d
Olaf <olaf.chuszno@gmail.com>
2023-10-02 17:08:23 +0200

    Create RegisterController

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        dd('register.create');
+    }
+
+    public function store()
+    {
+        dd('register.store');
+    }
+}
