4c3f32f0f034888c2529eb38dd1883bb254577b7
Olaf <olaf.chuszno@gmail.com>
2023-10-05 13:09:47 +0200

    Create test actions in SessionController

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
-    //
+    public function create()
+    {
+        dd('session.create');
+    }
+
+    public function store()
+    {
+        
+    }
 }
