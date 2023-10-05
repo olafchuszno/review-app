b20b1fb58a47a4e300e0f8c9f529cd664dc71156
Olaf <olaf.chuszno@gmail.com>
2023-10-05 21:55:58 +0200

    Style search.index view

--- resources/views/airport/index.blade.php
+++ resources/views/airport/index.blade.php
@@ -4,38 +4,38 @@
         <div class="flex justify-center mt-28">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 max-w-max">
                 <div class="py-2 align-middle min-w-full sm:px-6 lg:px-8">
-                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg" style="width: 820px">
-                        <table class="min-w-full divide-y divide-gray-200 mx-50">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200" width="820px">
                             <thead>
                                 <tr class="border-slate-300 border-2">
-                                    <th class="pl-4 py-2">Airport Code</th>
-                                    <th>Airport Name</th>
-                                    <th>Airport City</th>
-                                    <th class="pr-4 py-2">Airport Country</th>
+                                    <th class="pl-8 py-4 whitespace-nowrap">Airport Code</th>
+                                    <th class="py-2">Airport Name</th>
+                                    <th class="py-2">Airport City</th>
+                                    <th class="py-2">Airport Country</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 
                                 @foreach ($airports as $airport)
                                 <tr class="border-slate-300 border-2">
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    <td class="py-4 ml-2 whitespace-nowrap">
                                         <div class="flex justify-center">
                                             {{ $airport->code }}
                                         </div>
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    <td class="py-4 whitespace-nowrap">
                                         <div class="flex justify-center">
                                             <a href="airports/{{ $airport->code }}" class="text-blue-500">
                                                 {{ $airport->name }}
                                             </a>
                                         </div>
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    <td class="py-4 whitespace-nowrap">
                                         <div class="flex justify-center">
                                             {{ $airport->cityName }}
                                         </div>
                                     </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    <td class="py-4 whitespace-nowrap">
                                         <div class="flex justify-center">
                                             {{ $airport->countryName }}
                                         </div>
