895e31b7d969e0a6da2af87aa6e3466503638d6a
Olaf <olaf.chuszno@gmail.com>
2023-10-06 15:30:45 +0200

    Allow customizing size of logo component

--- resources/views/components/style/logo.blade.php
+++ resources/views/components/style/logo.blade.php
@@ -1,4 +1,6 @@
-<svg class="mx-auto aspect-auto" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100px" height="100px" viewBox="0 0 100 100" fill="#3f51b5">
+@props(['size' => '100px'])
+
+<svg class="mx-auto aspect-auto" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="{{ $size }}" height="{{ $size }}" viewBox="0 0 100 100" fill="#3f51b5">
     <!-- Curved Lines -->
     <path d="M30 60 C 30 30, 70 30, 70 60" stroke="#3f51b5" stroke-width="4" fill="none" />
     
