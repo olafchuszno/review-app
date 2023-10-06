9d760e4e4290123630ceea6f386e02b037132a53
Olaf <olaf.chuszno@gmail.com>
2023-10-06 15:32:22 +0200

    Use logo component in airport.show view

--- resources/views/airport/show.blade.php
+++ resources/views/airport/show.blade.php
@@ -3,15 +3,8 @@
 
 <main class="flex flex-col items-center justify-center mt-1">
 
-    <div class="flex justify-center">
-        <svg class="mx-auto aspect-auto" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100px" height="100px" viewBox="0 0 100 100" fill="#3f51b5">
-            <!-- Curved Lines -->
-            <path d="M30 60 C 30 30, 70 30, 70 60" stroke="#3f51b5" stroke-width="4" fill="none" />
-            
-            <!-- Circles -->
-            <circle cx="30" cy="60" r="5" fill="#ff9800" />
-            <circle cx="70" cy="60" r="5" fill="#ff9800" />
-        </svg>
+    <div>
+        <x-style.logo />
     </div>
 
     <div class="w-2/3 gap-2 flex flex-col items-center">
