496a235fa21eb4b91fc7860d03933eacbaaff04d
Olaf <olaf.chuszno@gmail.com>
2023-10-09 22:14:01 +0200

    Set migration for Topic

--- database/migrations/2023_10_09_200702_create_topics_table.php
+++ database/migrations/2023_10_09_200702_create_topics_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('airport_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
