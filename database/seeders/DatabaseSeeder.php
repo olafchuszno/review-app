b8ee8d8381da4f4d76211ba8b84863b3d5254f09
Olaf <olaf.chuszno@gmail.com>
2023-10-10 21:58:11 +0200

    Use sql file from local machine in db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,8 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $path = 'storage/app/public/gist.githubusercontent.com_tanerdogan_10103011_raw_1b7cf2b5dfcb9be242e7cc8fbc1b95950b16becb_airports.sql.txt';
-        DB::unprepared(file_get_contents($path));
+
+        DB::unprepared(file_get_contents('/Users/seiz/Downloads/sql/airports.sql'));
         $this->command->info('Airport table seeded!');
+
+        // User::factory(1)->create([
+        //     'first_name' => 'olaf',
+        //     'last_name' => 'olaf',
+        //     'email' => 'olaf@email.com',
+        // ]);
     }
 }
