cb456065377d9830a1683cad6ff900c16ff3693b
Olaf <olaf.chuszno@gmail.com>
2023-10-10 18:57:24 +0200

    Set migration for Answer

--- database/migrations/2023_10_09_200733_create_answers_table.php
+++ database/migrations/2023_10_09_200733_create_answers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('question_id')->constrained()->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade');
+            $table->string('body');
             $table->timestamps();
         });
     }
