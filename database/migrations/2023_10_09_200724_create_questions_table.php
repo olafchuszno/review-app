7ac8b222f9378ffa1b91de9d5bc41c6e1b824680
Olaf <olaf.chuszno@gmail.com>
2023-10-10 18:53:31 +0200

    Set migration for Question

--- database/migrations/2023_10_09_200724_create_questions_table.php
+++ database/migrations/2023_10_09_200724_create_questions_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('topic_id')->constrained()->onUpdate('cascade');
+            $table->string('body');
+            $table->string('description');
             $table->timestamps();
         });
     }
