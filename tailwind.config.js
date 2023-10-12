8605db7bff5e7456050d9956ede4889204ee8a66
Olaf <olaf.chuszno@gmail.com>
2023-10-12 19:48:31 +0200

    Save custom logo color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orangeLogo': '#ff9800',
+      },
+    },
   },
   plugins: [],
 }
