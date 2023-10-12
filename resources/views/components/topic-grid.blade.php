68f7d0e59c52a649fe9aae8a81c9b2a85eb0ddfd
Olaf <olaf.chuszno@gmail.com>
2023-10-12 19:49:55 +0200

    Create topic blade components

---
+++ resources/views/components/topic-grid.blade.php
@@ -0,0 +1,3 @@
+<div id="grid" class="w-2/5 gap-8 auto flex flex-wrap justify-between">
+    {{ $slot }}
+</div>
---
+++ resources/views/components/topic-link.blade.php
@@ -0,0 +1,5 @@
+<div class="text-2xl bg-indigo-700 text-orangeLogo font-bold px-6 py-6 rounded-2xl">
+    <a href="{{ $href }}">
+        {{ $slot }}
+    </a>
+</div>
