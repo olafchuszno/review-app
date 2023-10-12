a766267e6e17f5dafd1a3eec3129f83b8d97d631
Olaf <olaf.chuszno@gmail.com>
2023-10-12 19:58:13 +0200

    Pass Airport to the view in Topic.index

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Airport;
 use Illuminate\Http\Request;
 
 class TopicController extends Controller
 {
 
-    public function index()
+    public function index(Airport $airport)
     {
-        return view('topic.index');
+        return view('topic.index', [
+            'airport' => $airport
+        ]);
     }
 }
