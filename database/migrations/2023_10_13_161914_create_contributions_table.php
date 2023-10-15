aad96de6d170d08674936d4f6668cf43add26841
Olaf <olaf.chuszno@gmail.com>
2023-10-15 17:30:59 +0200

    Update Contribution migration

--- database/migrations/2023_10_13_161914_create_contributions_table.php
+++ database/migrations/2023_10_13_161914_create_contributions_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('contributions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('topic_id')->constrained();
             $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained();
+            $table->foreignId('airport_id')->constrained();
+            $table->foreignId('topic_id')->constrained();
             $table->string('variant')->nullable();
             $table->timestamps();
         });
