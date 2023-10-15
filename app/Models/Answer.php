c7c439ff59aa152a866d9906ce88d157e9b140c4
Olaf <olaf.chuszno@gmail.com>
2023-10-15 17:32:25 +0200

    Add foreign_id column to Answer relationship

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -19,6 +19,6 @@ class Answer extends Model
 
     public function question()
     {
-        return $this->belongsTo(Question::class);
+        return $this->belongsTo(Question::class, 'question_id');
     }
 }
