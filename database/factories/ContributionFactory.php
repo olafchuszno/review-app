938642f229573c1427d799c18dfa5b7890b3b50a
Olaf <olaf.chuszno@gmail.com>
2023-10-15 17:49:06 +0200

    Update factories

--- database/factories/ContributionFactory.php
+++ database/factories/ContributionFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Airport;
+use App\Models\Post;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -18,9 +20,13 @@ class ContributionFactory extends Factory
      */
     public function definition(): array
     {
+        $randomPost = rand(1, 10);
+
         return [
+            'user_id' => Post::find($randomPost)->user_id,
+            'post_id' => Post::find($randomPost)->id,
+            'airport_id' => Post::find($randomPost)->airport_id,
             'topic_id' => Topic::factory(),
-            'user_id' => User::factory(),
             'variant' => 'regular'
         ];
     }
