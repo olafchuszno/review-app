bfb5a6c9779b50e8f9fd1e44f966aad2f4dd2dca
Olaf <olaf.chuszno@gmail.com>
2023-10-22 10:53:36 +0200

    Change pagination to 3 per page

--- resources/views/contribution/index.blade.php
+++ resources/views/contribution/index.blade.php
@@ -1,24 +1,46 @@
 <x-layout>
 
-<x-main-logo class="flex flex-col items-center">
+<x-main-logo class="flex flex-col items-center mt-6 mb-20">
 
-    <div class="w-2/3 flex justify-center mt-10">
+    <div class="px-4 sm:px-0 flex flex-col items-center gap-3 mb-8">
+        <h1 class="text-center text-3xl font-semibold leading-7 ml-2">{{ $airport->name }} Page</h1>
+        
+        <div class="flex gap-1">
+            <x-section-links :airport="$airport"/>
+            <x-section-link href="" requestIs="*/topics*">{{ $topic->name }}</x-section-link>
+        </div>
+    </div>
+
+    <div class="flex flex-col w-2/3 items-center mt-8">
 
         @foreach ($contributions as $contribution)
 
-            <div class="mt-6 border-t border-gray-100">
+            <div class="p-5 border rounded-2xl border-slate-300 bg-slate-300 flex flex-col items-center mb-10">
 
-                <table class="divide-y divide-gray-100">
+                @if ($loop->first)
+                    <p class="py-3 text-2xl text-slate-200 w-2/5 bg-indigo-700  rounded-full font-semibold text-center my-5">
+                        The most relevant contribution
+                    </p>
+                @endif
+
+                <table class="divide-y divide-gray-100 text-indigo-800 mt-2">
                     
                     @foreach ($contribution->answers as $answer)
-                        <x-basic-info>
+                        <x-basic-info class="{{ $loop->last ? '' : 'border-b-2 border-indigo-300' }}">
                             <x-td-first>{{ $answer->question->body }}</x-td-first>
                             <x-td-second>{{ $answer->body }}</x-td-second>
                         </x-basic-info>
                     @endforeach
                     
                 </table>
+
             </div>
+
+            @if ($loop->first)
+                <p class="px-4 py-2 text-2xl text-slate-100 w-2/5 bg-slate-500 rounded-full font-semibold text-center mb-10">
+                    Other Contributions
+                </p>
+            @endif
 
         @endforeach
         
