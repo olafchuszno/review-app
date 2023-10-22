9d025fd8fd6a3f1cfd991a15e7a5975379b02deb
Olaf <olaf.chuszno@gmail.com>
2023-10-22 13:49:42 +0200

    Fix double password encryption

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Hashing\BcryptHasher;
 use Illuminate\Support\Str;
 
 /**
@@ -24,7 +25,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'avatar' => 'avatars/defaultAvatar.jpg',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password', // password
             'remember_token' => Str::random(10),
         ];
     }
