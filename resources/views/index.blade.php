88b4d609868466d15f7acaeb9b56a6c7f6b89c32
Olaf <olaf.chuszno@gmail.com>
2023-10-22 20:29:48 +0200

    Change homepage styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,9 @@
         <div class="mt-20">
             <x-style.logo size="150px"/>
         </div>
-        <div class="py-14 mx-auto max-w-xl rounded-full bg-indigo-600 flex flex-col gap-7 items-center">
+        <div class="py-14 px-4 mx-auto max-w-2xl rounded-full bg-primaryIndigo flex flex-col gap-7 items-center">
             <div>
-                <h2 class="text-3xl text-slate-100">
+                <h2 class="text-4xl font-bold text-stone-200">
                     Which airport are we looking for?
                 </h2>
             </div>
@@ -13,8 +13,8 @@
                 <form action="/search" method="POST" class="flex gap-4">
                     @csrf
 
-                    <input name="airport" placeholder="e.g. LAX or Warsaw" class="text-indigo-900 px-3 py-1 rounded-2xl border-2 bg-slate-200" type="text">
-                    <button type="submit" class="text-indigo-500 font-bold px-6 py-1 rounded-2xl bg-slate-200 ">Let's go!</button>
+                    <input name="airport" placeholder="e.g. LAX or Warsaw" class="text-indigo-900 text-lg placeholder:font-bold px-3 py-1 rounded-2xl border-2 bg-slate-200" type="text">
+                    <button type="submit" class="text-stone-200 font-bold px-6 py-1 rounded-2xl bg-primaryOrange">Let's go!</button>
                 </form>
             </div>
         </div>
