5b668d2b2e22fb0fcae7e785865dc5c9d7499fca
Olaf <olaf.chuszno@gmail.com>
2023-10-23 21:06:15 +0200

    Allow customizing classes

--- resources/views/components/section-link.blade.php
+++ resources/views/components/section-link.blade.php
@@ -1,3 +1,4 @@
-<a href="{{ $href }}" class="{{ request()->is($requestIs) ? 'bg-indigo-200 text-indigo-700' : '' }} mt-1 text-xl leading-6 font-bold rounded-lg hover:bg-indigo-200 hover:text-indigo-800 px-2 py-1">
+@props(['class' => '', 'href' => '', 'requestIs' => ''])
+<a href="{{ $href }}" class="{{ request()->is($requestIs) ? 'bg-indigo-200 text-indigo-700' : '' }} {{ $class }} mt-1 text-xl leading-6 font-bold rounded-lg hover:bg-indigo-200 hover:text-indigo-800 px-2 py-1">
     {{ $slot }}
 </a>
