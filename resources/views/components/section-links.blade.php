2f204faca92ac721bfd4c3758d15908113142348
Olaf <olaf.chuszno@gmail.com>
2023-10-23 21:07:24 +0200

    Add Contribute section link

--- resources/views/components/section-links.blade.php
+++ resources/views/components/section-links.blade.php
@@ -1,12 +1,21 @@
 <x-section-link 
     href="/airports/{{ $airport->code }}/basic_info"
     requestIs="*basic_info"
+    class="mr-1"
     >
     Basic info.
 </x-section-link>
 <x-section-link
     href="/airports/{{ $airport->code }}/main_topics" 
     requestIs="*main_topics"
+    class="mr-1"
 >
     Main topics.
+</x-section-link>
+<x-section-link
+    href="/airports/{{ $airport->code }}/create_contribution"
+    requestIs="*create_contribution"
+    class="mr-1"
+>
+    Contribute
 </x-section-link>
