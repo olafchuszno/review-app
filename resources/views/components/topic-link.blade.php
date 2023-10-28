463e26413cfa1725a16b9e277fd197da40f40953
Olaf <olaf.chuszno@gmail.com>
2023-10-28 15:41:45 +0200

    Allow changing classes in component & style

--- resources/views/components/topic-link.blade.php
+++ resources/views/components/topic-link.blade.php
@@ -1,6 +1,5 @@
-@props(['text' => 'text-primaryOrange', 'href'])
-<div class="text-2xl {{ $text }} bg-indigo-600 text-orangeLogo font-bold px-8 py-5 rounded-2xl">
-    <a href="{{ $href }}">
-        {{ $slot }}
-    </a>
-</div>
+@props(['class' => '', 'href'])
+
+<a href="{{ $href }}" class="text-2xl {{ $class }} bg-primaryIndigo text-white font-bold px-8 py-5 rounded-2xl">
+    {{ $slot }}
+</a>
