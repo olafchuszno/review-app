9d0bdb2f4a9405b51db20289ffcf05f4678914b1
Olaf <olaf.chuszno@gmail.com>
2023-10-28 15:39:11 +0200

    Make links clickable on the whole area & style

--- resources/views/topic/index.blade.php
+++ resources/views/topic/index.blade.php
@@ -1,10 +1,18 @@
 <x-layout>
     <x-main-airport :airport="$airport">
 
+        <h1 class="text-primaryIndigo text-2xl font-bold mb-12 px-4 py-2 rounded-2xl bg-neutralIndigo">
+            What information are you looking for?
+        </h1>
+
         <x-topic-grid>
 
             @foreach ($topics as $topic)
-                <x-topic-link href="/airports/{{ $airport->code }}/topics/{{ $topic->name }}">{{ $topic->name }}</x-topic-link>
+                <x-topic-link href="/airports/{{ $airport->code }}/topics/{{ $topic->name }}"
+                    class="hover:bg-neutralIndigo hover:text-primaryOrange"    
+                >
+                    {{ $topic->name }}
+                </x-topic-link>
             @endforeach
 
         </x-topic-grid>
