1a6d2aaa0088174b67b3cd01eac730125564022a
Olaf <olaf.chuszno@gmail.com>
2023-10-29 13:55:23 +0100

    Add unique and index on Question->short

--- database/migrations/2023_10_09_200724_create_questions_table.php
+++ database/migrations/2023_10_09_200724_create_questions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('topic_id')->constrained()->onUpdate('cascade');
-            $table->string('short');
+            $table->string('short')->unique('short');
             $table->string('body');
             $table->text('description');
             $table->timestamps();
