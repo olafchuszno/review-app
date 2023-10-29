bbc247018cc48c3fb11e2f37c28112b50f2d5f9d
Olaf <olaf.chuszno@gmail.com>
2023-10-29 20:11:26 +0100

    Create basic Contribution.store controller

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Airport;
+use App\Models\Answer;
 use App\Models\Contribution;
+use App\Models\Question;
 use App\Models\Topic;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ContributionController extends Controller
 {
@@ -34,6 +38,44 @@ class ContributionController extends Controller
             'topic' => $topic,
             'questions' => $topic->questions
         ]);
+    }
+
+    public function store(Airport $airport, Topic $topic)
+    {
+        // Get ids of all questions that belong to the topic
+        $question_shorts = $topic->questions->pluck('short');
+
+        // Get ids of all questions that belong to the topic
+        $question_ids = $topic->questions->pluck('id');
+        
+        
+        // Validate the answers
+        foreach($question_shorts as $question_short) {
+
+            $answers[] = request()->validate([
+                $question_short => ['required', 'min:1', 'max:500']
+            ]);
+        }
+
+        // Create the Contribution
+        $contribution = Contribution::factory()->create([
+            'user_id' => auth()->id(),
+            'airport_id' => $airport->id,
+            'topic_id' => $topic->id,
+            'variant' => 'regular'
+        ]);
+
+        // Create the Answers
+        foreach (range(0, count($answers)-1) as $x) {
+
+            Answer::factory()->create([
+                'contribution_id' => $contribution->id,
+                'question_id' => $question_ids[$x],
+                'body' => array_values($answers[$x])[0]
+            ]);
+        }
+
+        return redirect('/')->with('success', 'Contribution was published. Thank you for your input!');
     }
 
 }
