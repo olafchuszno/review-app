74fea3fc74c7d731be13f95f9ad48c099b325b5f
Olaf <olaf.chuszno@gmail.com>
2023-11-01 17:04:08 +0100

    Delete 'short' from Question migration

--- database/migrations/2023_10_09_200724_create_questions_table.php
+++ database/migrations/2023_10_09_200724_create_questions_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('topic_id')->constrained()->onUpdate('cascade');
-            $table->string('short')->unique('short');
             $table->string('body');
             $table->text('description');
             $table->timestamps();
