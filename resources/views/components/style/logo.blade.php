10a976dd71ba843c6a156dbff239cc4c54ea2aa7
Olaf <olaf.chuszno@gmail.com>
2023-11-04 13:03:07 +0100

    Update Logo

--- resources/views/components/style/logo.blade.php
+++ resources/views/components/style/logo.blade.php
@@ -2,9 +2,9 @@
 
 <svg class="mx-auto aspect-auto" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="{{ $size }}" height="{{ $size }}" viewBox="0 0 100 100" fill="#3f51b5">
     <!-- Curved Lines -->
-    <path d="M30 60 C 30 30, 70 30, 70 60" stroke="#3f51b5" stroke-width="4" fill="none" />
+    <path d="M30 60 C 30 30, 70 30, 70 60" stroke="#7e00a8" stroke-width="4" fill="none" />
     
     <!-- Circles -->
-    <circle cx="30" cy="60" r="5" fill="#f97316" />
-    <circle cx="70" cy="60" r="5" fill="#f97316" />
+    <circle cx="30" cy="60" r="5" fill="#ff6200" />
+    <circle cx="70" cy="60" r="5" fill="#ff6200" />
 </svg>
