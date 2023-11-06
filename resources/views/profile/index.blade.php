576c24f590b7a223d0ae380709ef7cfd158e9ddd
Olaf <olaf.chuszno@gmail.com>
2023-11-06 14:56:53 +0100

    Style

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -69,7 +69,7 @@
                         </tr>
                         <tr class="flex justify-evenly py-1 mt-6 mb-2">
                             <td>
-                                <a href="/profile/edit" class="bg-secondaryIndigo text-primaryOrange text-lg font-bold px-5 py-2 rounded-xl hover:bg-secondaryIndigo hover:border hover:border-primaryOrange">
+                                <a href="/profile/edit" class="bg-secondaryIndigo border font-medium hover:bg-secondaryIndigo hover:border hover:border-primaryOrange px-5 py-2 rounded-xl text-lg">
                                     Edit your profile
                                 </a>
                             </td>
