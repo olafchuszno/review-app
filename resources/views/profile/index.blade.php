5341b3e429e25484b9414844e5cc0d75c7210a72
Olaf <olaf.chuszno@gmail.com>
2023-11-06 12:07:05 +0100

    Change link href

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -64,7 +64,7 @@
                                 Email
                             </td>
                             <td class="p-2 text-lg font-semibold text-center">
-                                <a href="change-username" class="flex justify-center items-center gap-1 hover:text-primaryOrange">
+                                <a href="change-email" class="flex justify-center items-center gap-1 hover:text-primaryOrange">
                                     {{ auth()->user()->email }}
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="#ff6200" d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z"/></svg>
                                 </a>
