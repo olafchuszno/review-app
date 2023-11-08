8557b5dd89257d895798ef37cee05ae22b7ef83c
Olaf <olaf.chuszno@gmail.com>
2023-11-08 14:25:05 +0100

    Implement profile.update action

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class ProfileController extends Controller
 {
@@ -19,5 +21,42 @@ class ProfileController extends Controller
             'avatar' => auth()->user()->avatar,
             'username' => auth()->user()->username
         ]);
+    }
+
+    public function update()
+    {
+        $user = User::find(auth()->user()->id);
+
+        // Validate the input
+        $attributes = request()->validate([
+            'first_name' => ['required', 'max:255', 'alpha_num:ascii'],
+            'last_name' => ['required', 'max:255', 'alpha_num:ascii'],
+            'avatar' => 'image',
+            'username' => ['required', 'min:4', 'max:255', "unique:users,username,{$user->id}", 'alpha_num:ascii' ],
+            'password' => ['required']
+        ]);
+
+        if (Hash::check($attributes['password'], $user->password)) {
+
+            unset($attributes['password']);
+        } else {
+
+            throw ValidationException::withMessages([
+                'password' => 'Incorrect password'
+            ]);
+
+            die();
+        }
+
+        // Store the avatar (if set)
+        if (isset($attributes['avatar'])) {
+
+            $attributes['avatar'] = request()->file('avatar')->store('avatars');
+        } 
+
+        // Update the profile
+        $user->update($attributes);
+
+        return redirect('/profile')->with('success', 'Your profile was updated');
     }
 }
