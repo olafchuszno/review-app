26afc6661bbb25c0f70ddf19283a216119b658b6
Olaf <olaf.chuszno@gmail.com>
2023-11-08 16:00:15 +0100

    Create profile.menu blade component

---
+++ resources/views/components/profile/menu.blade.php
@@ -0,0 +1,17 @@
+<ul role="list" class="w-full flex flex-col gap-4">
+    <x-profile.li href="profile">
+        Profile
+    </x-profile.li>
+
+    <x-profile.li href="account">
+        Account
+    </x-profile.li>
+
+    <x-profile.li href="posts">
+        Posts
+    </x-profile.li>
+
+    <x-profile.li href="contributions">
+        Contributions
+    </x-profile.li>
+</ul>
