894d510a26851a7b31061004f30b14319d060ad3
Olaf <olaf.chuszno@gmail.com>
2023-11-09 11:14:09 +0100

    Create basic Account.edit action

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
@@ -9,5 +10,12 @@ class AccountController extends Controller
     public function index()
     {
         return view('account.index');
+    }
+
+    public function edit()
+    {
+        return view('account.edit', [
+            'user' => User::find(auth()->user()->id)
+        ]);
     }
 }
