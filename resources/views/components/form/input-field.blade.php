0a3094954c00cc630a6e6952c7f1706981dd5d5f
Olaf <olaf.chuszno@gmail.com>
2023-11-09 15:26:03 +0100

    Update style of input component

--- resources/views/components/form/input-field.blade.php
+++ resources/views/components/form/input-field.blade.php
@@ -1,7 +1,7 @@
-@props(['type' => 'text', 'label_class' => '', 'input_class' => 'text-indigo-700 placeholder:text-indigo-700', 'name', 'div_class' => ''])
+@props(['type' => 'text', 'label_class' => '', 'input_class' => 'text-indigo-700 placeholder:text-indigo-700', 'name', 'div_class' => '',  'error_case' => $errors->has($name) ? 'ring-red-500 ring-2' : 'focus:ring-indigo-600 ring-1'])
 <div class="{{ $div_class }}">
     <label for="{{ $name }}" class="text-neutralStone mb-1 block text-lg font-semibold leading-6 {{ $label_class }}">{{ $slot }}</label>
     <div class="mt-0.5">
-    <input id="{{ $name }}" name="{{ $name }}" type="{{ $type }}" {{ $attributes }} class="{{ $input_class }} block w-full text-lg rounded-md bg-neutralStone border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset font-semibold ring-neutralStone focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+    <input id="{{ $name }}" name="{{ $name }}" type="{{ $type }}" {{ $attributes }} class="{{ $input_class }} block w-full text-lg {{ $error_case }} rounded-md border-0 py-1.5 px-2 outline-none shadow-sm ring-inset font-semibold bg-neutralStone ring-neutralStone focus:ring-2 focus:ring-inset sm:text-lg sm:leading-6">
     </div>
 </div>
