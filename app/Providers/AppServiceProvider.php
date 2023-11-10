904ed61ff2b6f2bd9b71389da410f84cda31cf56
Olaf <olaf.chuszno@gmail.com>
2023-11-10 13:36:13 +0100

    Create default password rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Password::defaults(function () {
+
+        $rule = Password::min(8)->mixedCase()->numbers()->symbols();
+
+        /** @var \Illuminate\Foundation\Application $app */
+        $app = $this->app;
+ 
+        return $app->isProduction()
+                    ? $rule->uncompromised()
+                    : $rule;
+    });
     }
 }
