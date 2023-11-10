d10d3c654c974bece9a6860552dbf50e34e95b29
Olaf <olaf.chuszno@gmail.com>
2023-11-10 13:35:49 +0100

    Create basic Account.update action

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
@@ -18,4 +19,25 @@ class AccountController extends Controller
             'user' => User::find(auth()->user()->id)
         ]);
     }
+
+    public function update()
+    {
+        // Validate input
+        $attributes = request()->validate([
+            'current_password' => ['required', 'min:8', 'current_password'],
+            'new_password' => ['required', 'max:255', Password::default(), 'confirmed'],
+            'new_password_confirmation' => ['required']
+        ]);
+
+        $new_password = [
+            'password' => $attributes['new_password']
+        ];
+
+        // Update User's password
+        User::find(auth()->user()->id)->update($new_password);
+
+        return redirect('/account')->with('success', 'Password was updated!');
+    }
+
+
 }
