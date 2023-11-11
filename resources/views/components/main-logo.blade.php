51921db86d4b8e177e1643fd0f3a0bb5ac9edc29
Olaf <olaf.chuszno@gmail.com>
2023-11-11 18:51:18 +0100

    Refactor main-logo component

--- resources/views/components/main-logo.blade.php
+++ resources/views/components/main-logo.blade.php
@@ -1,9 +1,15 @@
-@props(['class' => ''])
+@props(['class' => '', 'heading'])
 <main class="mt-1 {{ $class }}">
+    <section>
 
-    <div>
-        <x-style.logo />
-    </div>
+        <div>
+            <x-style.logo />
+        </div>
+
+        <x-h1-so>
+            {{ $heading }}
+        </x-h1-so>
+    </section>
 
     {{ $slot }}
 
