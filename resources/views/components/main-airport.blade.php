7f386181274607ac7819863b7737ea1f3e9b1f17
Olaf <olaf.chuszno@gmail.com>
2023-11-11 18:53:06 +0100

    Use h1-so component in the view

--- resources/views/components/main-airport.blade.php
+++ resources/views/components/main-airport.blade.php
@@ -1,7 +1,9 @@
 <x-main-logo class="flex flex-col items-center mt-6 mb-20">
 
-    <div class="px-4 sm:px-0 flex flex-col items-center gap-4 mb-10">
-        <h1 class="text-center text-secondaryOrange text-4xl font-bold leading-7 ml-2">{{ $airport->name }} Page</h1>
+    <section class="px-4 sm:px-0 flex flex-col items-center gap-4 mb-10">
+        <x-h1-so>
+            {{ $airport->name }} Page
+        </x-h1-so>
         
         <div class="flex gap-1">
             <x-section-links :airport="$airport"/>
@@ -10,7 +12,7 @@
                 <x-section-link href="" requestIs="*/topics*">{{ $topic->name }}</x-section-link>
             @endif
         </div>
-    </div>
+    </section>
 
     {{ $slot }}
     
