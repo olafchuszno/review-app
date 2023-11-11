1cfd9a750734538b9c607fa346cac0288304219f
Olaf <olaf.chuszno@gmail.com>
2023-11-11 18:54:27 +0100

    Create MyContributionController & index action

---
+++ app/Http/Controllers/MyContributionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Contribution;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MyContributionController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(auth()->user()->id);
+        
+        // Display all user's contributions, paginated to 6 results per page.
+        return view('my_contribution.index', [
+            'contributions' => User::find(auth()->user()->id)->contributions->paginate(3)
+        ]);
+    }
+}
