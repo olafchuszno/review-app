7c94ac798e20984f133bf0eacb96deffdf9ff544
Olaf <olaf.chuszno@gmail.com>
2023-11-13 13:25:06 +0100

    Add heading to component in the view

--- resources/views/components/main-airport.blade.php
+++ resources/views/components/main-airport.blade.php
@@ -1,10 +1,6 @@
-<x-main-logo class="flex flex-col items-center mt-6 mb-20">
+<x-main-logo heading="{{$airport->name}} Page" class="flex flex-col items-center mt-6 mb-20">
 
-    <section class="px-4 sm:px-0 flex flex-col items-center gap-4 mb-10">
-        <x-h1-so>
-            {{ $airport->name }} Page
-        </x-h1-so>
-        
+    <section class="px-4 sm:px-0 flex flex-col items-center gap-4 mt-4 mb-10">
         <div class="flex gap-1">
             <x-section-links :airport="$airport"/>
 
