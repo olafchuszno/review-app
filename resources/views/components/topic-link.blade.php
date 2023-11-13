74590aa5fd1455fd9bc5e6f0dfb5f876356bc3e8
Olaf <olaf.chuszno@gmail.com>
2023-11-13 13:24:19 +0100

    Update component style

--- resources/views/components/topic-link.blade.php
+++ resources/views/components/topic-link.blade.php
@@ -1,5 +1,5 @@
 @props(['class' => '', 'href'])
 
-<a href="{{ $href }}" class="text-2xl {{ $class }} bg-primaryIndigo text-white font-bold px-8 py-5 rounded-2xl hover:bg-neutralIndigo hover:text-primaryOrange">
+<a href="{{ $href }}" class="text-2xl {{ $class }} bg-primaryIndigo text-white font-bold px-8 py-5 rounded-2xl hover:bg-neutralStone hover:text-primaryIndigo">
     {{ $slot }}
 </a>
