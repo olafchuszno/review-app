6f6d1944ffeeabec536ebc6c4d2fe171a8fd85d0
Olaf <olaf.chuszno@gmail.com>
2023-11-14 13:32:52 +0100

    Allow blade component class customization

--- resources/views/components/basic-info.blade.php
+++ resources/views/components/basic-info.blade.php
@@ -1,4 +1,4 @@
-@props(['class' => 'border-b-2 border-slate-200'])
-<tr class="{{ $class }} px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+@props(['class' => 'border-b-2 border-stone-200 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'])
+<tr class="{{ $class }}">
     {{ $slot }}
 </tr>
