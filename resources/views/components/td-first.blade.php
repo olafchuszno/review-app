803a237c7b851b78be6c402a9b5e35e94bf18e7c
Olaf <olaf.chuszno@gmail.com>
2023-11-14 13:32:30 +0100

    Allow blade component class customization

--- resources/views/components/td-first.blade.php
+++ resources/views/components/td-first.blade.php
@@ -1,3 +1,4 @@
-<td class="py-2 px-2 font-extrabold text-lg leading-6 mr-5">
+@props(['class' => 'py-2 px-2 font-extrabold text-lg leading-6 mr-5'])
+<td class="{{ $class }}">
     {{ $slot }}
 </td>
--- resources/views/components/td-second.blade.php
+++ resources/views/components/td-second.blade.php
@@ -1,3 +1,4 @@
-<td class="mt-1 py-2 text-base font-semibold leading-6 sm:col-span-2 sm:mt-0">
+@props(['class' => 'mt-1 py-2 text-base font-semibold leading-6 sm:col-span-2 sm:mt-0'])
+<td class="{{ $class }}">
     {{ $slot }}
 </td>
