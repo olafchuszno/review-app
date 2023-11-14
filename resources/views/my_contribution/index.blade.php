5a71050dea56968e175bfd902d6481131cc370e5
Olaf <olaf.chuszno@gmail.com>
2023-11-14 13:25:43 +0100

    Show a message&hide content if none contributions

--- resources/views/my_contribution/index.blade.php
+++ resources/views/my_contribution/index.blade.php
@@ -1,18 +1,19 @@
 <x-layout>
-    <x-main-logo heading="Your contributions!">
+    <x-main-logo heading="{{$contributions->count() ? 'Your contributions!' : 'You have not contributed yet.'}}">
 
-        <section class="flex flex-col items-center mt-8">
+        @if ($contributions->count())
+            <section class="flex flex-col items-center mt-8">
 
-            <x-navigation.buttons :contributions="$contributions" />
+                <x-navigation.buttons :contributions="$contributions" />
 
-            <x-contribution.short-grid :contributions="$contributions" />
+                <x-contribution.short-grid :contributions="$contributions" />
 
 
-            <div class="flex items-center mt-10">
-                {{ $contributions->links() }}
-            </div>
+                <div class="flex items-center mt-10">
+                    {{ $contributions->links() }}
+                </div>
 
-        </section>
-        
+            </section>
+        @endif
     </x-main-airport>
 </x-layout>
