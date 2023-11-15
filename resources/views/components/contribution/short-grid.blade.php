16d3b4223031e49c96f898e09ec2c4dc2d5e6dce
Olaf <olaf.chuszno@gmail.com>
2023-11-15 16:20:02 +0100

    Update view's style

--- resources/views/components/contribution/short-grid.blade.php
+++ resources/views/components/contribution/short-grid.blade.php
@@ -8,13 +8,13 @@
 
             <table class="text-primaryIndigo mt-2">
                 
-                <tr class="lg:grid gap-6 lg:grid-cols-3 font-bold p-4">
-                    <td class="col-span-1 lg:text-lg sm:text-md text-center">
+                <tr class="p-2 gap-4 font-bold lg:grid lg:grid-cols-3">
+                    <td class="col-span-1 lg:text-lg p-2 sm:text-md text-center">
                         <a href="contributions/{{ $contribution->id }}">
                             {{ $contribution->answers->first()->question->body }}
                         </a>
                     </td>
-                    <td class="col-span-2 lg:text-sm sm:text-xs">
+                    <td class="col-span-2 pb-1 sm:text-md">
                         <a href="contributions/{{ $contribution->id }}">
                             {{ $contribution->answers->first()->body }}
                         </a>
