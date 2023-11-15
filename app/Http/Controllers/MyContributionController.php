3810bd9d0789c6f4a774775db94e358cdaa1f7d2
Olaf <olaf.chuszno@gmail.com>
2023-11-15 12:03:44 +0100

    Create MyContribution.show action

--- app/Http/Controllers/MyContributionController.php
+++ app/Http/Controllers/MyContributionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Contribution;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,6 +15,20 @@ class MyContributionController extends Controller
         // Display all user's contributions, paginated to 6 results per page.
         return view('my_contribution.index', [
             'contributions' => User::find(auth()->user()->id)->contributions->paginate(8)
+        ]);
+    }
+
+    public function show(Contribution $contribution)
+    {
+        // Check for authorization to see the contribution
+        if (auth()->user()->id != $contribution->author->id) {
+
+            return throw new AuthorizationException('This action is forbidden', 403);
+        }
+
+        // Display the (user's) contribution
+        return view('my_contribution.show', [
+            'contribution' => $contribution->with('answers')->get()->first()
         ]);
     }
 }
