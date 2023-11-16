49388563b6e193addd2e59e873afe297cfa423d2
Olaf <olaf.chuszno@gmail.com>
2023-11-16 15:15:33 +0100

    Create getUser static methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public static function getUser()
+    {
+        return auth()->user();
+    }
+
+    public static function getUserId()
+    {
+        return auth()->user()->id;
+    }
 }
