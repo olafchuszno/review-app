b8e988f502d0ce5c9d686b456dfacc447bbd14b2
Olaf <olaf.chuszno@gmail.com>
2023-11-16 11:07:46 +0100

    Make navigation.buttons allow any object

--- resources/views/components/navigation/buttons.blade.php
+++ resources/views/components/navigation/buttons.blade.php
@@ -1,17 +1,17 @@
-@props(['contributions', 'class' => ''])
+@props(['object', 'class' => ''])
 <div class="{{ $class }}">
-    @if ($contributions->hasPages())
+    @if ($object->hasPages())
         <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between gap-4">
             {{-- Previous Page Link --}}
-            @if (! $contributions->onFirstPage())
-                <a href="{{ $contributions->previousPageUrl() }}" rel="prev" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-neutralStone bg-primaryIndigo leading-5 rounded-md hover:text-primaryOrange focus:outline-none focus:ring ring-neutralStone focus:border-blue-300 active:bg-neutralStone active:text-primaryIndigo transition ease-in-out duration-150">
+            @if (! $object->onFirstPage())
+                <a href="{{ $object->previousPageUrl() }}" rel="prev" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-neutralStone bg-primaryIndigo leading-5 rounded-md hover:text-primaryOrange focus:outline-none focus:ring ring-neutralStone focus:border-blue-300 active:bg-neutralStone active:text-primaryIndigo transition ease-in-out duration-150">
                     {!! __('pagination.previous') !!}
                 </a>
             @endif
 
             {{-- Next Page Link --}}
-            @if ($contributions->hasMorePages())
-                <a href="{{ $contributions->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-neutralStone bg-primaryIndigo leading-5 rounded-md hover:text-primaryOrange focus:outline-none focus:ring ring-neutralStone focus:border-blue-300 active:bg-neutralStone active:text-primaryIndigo transition ease-in-out duration-150">
+            @if ($object->hasMorePages())
+                <a href="{{ $object->nextPageUrl() }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-neutralStone bg-primaryIndigo leading-5 rounded-md hover:text-primaryOrange focus:outline-none focus:ring ring-neutralStone focus:border-blue-300 active:bg-neutralStone active:text-primaryIndigo transition ease-in-out duration-150">
                     {!! __('pagination.next') !!}
                 </a>
             @endif
