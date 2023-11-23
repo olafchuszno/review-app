b4f84374c6993283639a66706a8490df7d6a620a
Olaf <olaf.chuszno@gmail.com>
2023-11-23 22:28:13 +0300

    Add onDelete to Answers table

--- database/migrations/2023_10_15_200733_create_answers_table.php
+++ database/migrations/2023_10_15_200733_create_answers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('contribution_id')->constrained();
+            $table->foreignId('contribution_id')->constrained()->onDelete('cascade');
             $table->foreignId('question_id')->constrained();
             $table->text('body');
             $table->timestamps();
