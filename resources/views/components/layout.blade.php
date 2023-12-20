5eabd05387d72411d811b86057a0117f4a1a880f
Olaf <olaf.chuszno@gmail.com>
2023-12-20 22:58:08 +0300

    Add a trigger id

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -72,7 +72,7 @@
                     <div class="relative ml-3">
                         <div>
                             <button id="dropdown-menu-button" type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
+                                <span id="trigger" class="absolute -inset-1.5"></span>
                                 <span class="sr-only">Open user menu</span>
                                 <img class="h-8 w-8 rounded-full" src="{{ asset('storage/' . auth()->user()->avatar) }}" alt="users avatar">
                             </button>
