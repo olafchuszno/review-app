a0b519fea9b03af5c321c11c28445da70cb2e63a
Olaf <olaf.chuszno@gmail.com>
2023-12-22 23:21:50 +0300

    Fix overflowing answer body in short-grid comp

--- resources/views/components/contribution/short-grid.blade.php
+++ resources/views/components/contribution/short-grid.blade.php
@@ -4,27 +4,27 @@
 
     @foreach ($contributions as $contribution)
 
-        <div class="col-span-3 border rounded-2xl border-neutralStone bg-indigo-300">
+    <div class="col-span-3 border rounded-2xl border-neutralStone bg-indigo-300">
 
-            <table class="text-primaryIndigo mt-2">
-                
-                <tr class="p-2 gap-4 font-bold lg:grid lg:grid-cols-3">
-                    <td class="col-span-1 lg:text-lg p-2 sm:text-md text-center">
-                        <a href="contributions/{{ $contribution->id }}">
-                            {{ $contribution->answers->first()->question->body }}
-                        </a>
-                    </td>
-                    <td class="col-span-2 pb-1 sm:text-md">
-                        <a href="contributions/{{ $contribution->id }}">
-                            {{ $contribution->answers->first()->body }}
-                        </a>
-                    </td>
-                </tr>
-                
-            </table>
+        <table class="text-primaryIndigo mt-2">
 
-        </div>
+            <tr class="p-2 gap-4 font-bold lg:grid lg:grid-cols-3">
+                <td class="col-span-1 lg:text-lg p-2 sm:text-md text-center">
+                    <a href="contributions/{{ $contribution->id }}">
+                        {{ $contribution->answers->first()->question->body }}
+                    </a>
+                </td>
+                <td class="col-span-2 pb-1 sm:text-md overflow-hidden">
+                    <a href="contributions/{{ $contribution->id }}" class="break-all">
+                        {{ $contribution->answers->first()->body }}
+                    </a>
+                </td>
+            </tr>
+
+        </table>
+
+    </div>
 
     @endforeach
-    
+
 </div>
