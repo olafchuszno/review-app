02fcc454274b8149b0a6759831cbc9f205d585fa
Olaf <olaf.chuszno@gmail.com>
2023-12-23 20:19:25 +0300

    Fix most relevant contribution tag not showing

--- resources/views/contribution/index.blade.php
+++ resources/views/contribution/index.blade.php
@@ -1,48 +1,42 @@
 <x-layout>
 
-<x-main-airport :airport="$airport" :topic="$topic">
+    <x-main-airport :airport="$airport" :topic="$topic">
 
-    <section class="flex flex-col w-2/3 items-center">
+        <section class="flex flex-col w-2/3 items-center">
 
-    <div class="mb-4">
-        <x-navigation.buttons :object="$contributions"/>
-    </div>
+            <div class="mb-4">
+                <x-navigation.buttons :object="$contributions" />
+            </div>
 
-        @foreach ($contributions as $contribution)
+            @foreach ($contributions as $contribution)
 
             <div class="p-5 border rounded-2xl border-neutralStone bg-indigo-300 flex flex-col items-center mb-10">
 
-                @if (request('page') == 1 && $loop->first)
-                    <p class="py-3 text-2xl text-stone-200 w-2/5 bg-secondaryOrange font-bold rounded-full text-center my-5">
-                        The most relevant contribution
-                    </p>
+                @if ((request('page') == 1 || request('page') == null) && $loop->first)
+                <p class="py-3 text-2xl text-stone-200 w-2/5 bg-primaryIndigo font-bold rounded-full text-center my-5">
+                    The most relevant contribution
+                </p>
                 @endif
 
                 <table class="divide-y divide-gray-100 text-indigo-800 mt-2">
-                    
+
                     @foreach ($contribution->answers as $answer)
-                        <x-basic-info class="{{ $loop->last ? '' : 'border-b-2 border-indigo-200' }}">
-                            <x-td-first>{{ $answer->question->body }}</x-td-first>
-                            <x-td-second>{{ $answer->body }}</x-td-second>
-                        </x-basic-info>
+                    <x-basic-info class="{{ $loop->last ? '' : 'border-b-2 border-indigo-200' }}">
+                        <x-td-first>{{ $answer->question->body }}</x-td-first>
+                        <x-td-second>{{ $answer->body }}</x-td-second>
+                    </x-basic-info>
                     @endforeach
-                    
+
                 </table>
 
             </div>
 
-            @if (request('page') == 1 && $loop->first)
-                <p class="px-4 py-2 text-2xl text-slate-100 w-2/5 bg-slate-500 rounded-full font-semibold text-center mb-10">
-                    Other Contributions
-                </p>
-            @endif
+            @endforeach
 
-        @endforeach
-        
-    </section>
+        </section>
 
-    {{ $contributions->links() }}
-    
-</x-main-airport>
+        {{ $contributions->links() }}
+
+    </x-main-airport>
 
 </x-layout>
