da746e957b066fec46287e37fac54b2ee6b648d6
Olaf <olaf.chuszno@gmail.com>
2023-12-23 20:09:12 +0300

    Set relationships for Like model

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function contribution()
+    {
+        return $this->belongsTo('contribution', 'contribution_id');
+    }
 }
