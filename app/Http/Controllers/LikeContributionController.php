b3c495a8f404125506216ab26b798e8f22f30bde
Olaf <olaf.chuszno@gmail.com>
2023-12-26 22:32:38 +0300

    Implement contribution like functionality (basic)

--- app/Http/Controllers/LikeContributionController.php
+++ app/Http/Controllers/LikeContributionController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contribution;
+use App\Models\Like;
 use Illuminate\Http\Request;
 
 class LikeContributionController extends Controller
 {
-    public function store()
+    public function store(Contribution $contribution)
     {
-        return view('page_under_construction');
+        // Check whether the contribution exists and the like doesn't exist yet
+        if (Contribution::find($contribution->id) != null && !Like::where([
+            'user_id' => auth()->id(),
+            'contribution_id' => $contribution->id
+        ])->get()->count()) {
+
+            // Like the contribution
+            Like::factory()->create([
+                'user_id' => auth()->id(),
+                'contribution_id' => $contribution->id
+            ]);
+
+            // Back with success message
+            return back()->with('success', 'You liked the contribution');
+
+            // Else the contribution doesn't exist
+        } else {
+
+            return back()->with('failure', "Couldn't like the contribution");
+        }
     }
 }
