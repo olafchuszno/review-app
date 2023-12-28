320d10b2803db72cb30bfa16d3364a68e81d23c9
Olaf <olaf.chuszno@gmail.com>
2023-12-28 22:05:07 +0300

    style (h1 to raisinBlack)

--- resources/views/contribution/choose_category.blade.php
+++ resources/views/contribution/choose_category.blade.php
@@ -1,19 +1,19 @@
 <x-layout>
     <x-main-airport :airport="$airport">
 
-        <h1 class="text-primaryIndigo text-2xl font-bold mb-12 px-4 py-2 rounded-2xl bg-neutralStone">
+        <h1 class="text-primaryIndigo text-2xl font-bold mb-12 px-4 py-2 rounded-2xl bg-raisinBlack">
             Choose the category of your contribution
         </h1>
 
         <x-topic-grid>
 
             @foreach ($topics as $topic)
-                <x-topic-link text="text-primaryOrange" href="/airports/{{ $airport->code }}/contribute/{{ $topic->name }}">
-                    {{ $topic->name }}
-                </x-topic-link>
+            <x-topic-link text="text-primaryOrange" href="/airports/{{ $airport->code }}/contribute/{{ $topic->name }}">
+                {{ $topic->name }}
+            </x-topic-link>
             @endforeach
 
         </x-topic-grid>
-        
+
     </x-main-airport>
 </x-layout>
