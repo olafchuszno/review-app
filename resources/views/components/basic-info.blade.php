6f6d8ab118640aafbe0bae4b54a08933fcff43fa
Olaf <olaf.chuszno@gmail.com>
2023-12-28 22:14:05 +0300

    Update basic-info component (style - borders)

--- resources/views/components/basic-info.blade.php
+++ resources/views/components/basic-info.blade.php
@@ -1,4 +1,4 @@
-@props(['class' => 'border-b-2 border-stone-200 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'])
-<tr class="{{ $class }}">
+@props(['border' => 'border-b-2', 'class' => 'border-raisinBlack px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'])
+<tr class="{{ $class }} {{ $border }}">
     {{ $slot }}
 </tr>
