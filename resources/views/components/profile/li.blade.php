bf2dbf4c9a09f4e183677c4471a68700d813c21d
Olaf <olaf.chuszno@gmail.com>
2023-12-28 21:38:34 +0300

    Update style

--- resources/views/components/profile/li.blade.php
+++ resources/views/components/profile/li.blade.php
@@ -1,6 +1,6 @@
 @props(['href' => ''])
 <li class="flex justify-center text-center -mb-0.5">
-    <a href="/{{ $href }}" class="text-md w-full rounded-2xl py-2 font-bold leading-6 bg-primaryIndigo {{ request()->is($href) ? 'text-primaryOrange bg-secondaryIndigo shadow-sm shadow-primaryIndigo border-primaryIndigo border-2' : 'text-stone-300  hover:text-primaryOrange hover:bg-secondaryIndigo hover:border-2 hover:border-primaryIndigo hover:shadow-sm hover:shadow-primaryIndigo'}}">
+    <a href="/{{ $href }}" class="text-md w-full rounded-2xl py-2 font-bold leading-6 bg-primaryIndigo {{ request()->is($href) ? 'text-primaryIndigo bg-raisinBlack shadow-sm shadow-primaryIndigo border-primaryIndigo border-2' : 'text-raisinBlack  hover:text-primaryIndigo hover:bg-raisinBlack hover:shadow-sm hover:shadow-primaryIndigo'}}">
         {{ $slot }}
     </a>
 </li>
