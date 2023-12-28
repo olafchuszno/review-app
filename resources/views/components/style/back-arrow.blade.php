c99f169b4bfc8c8a66d55bf7b9c0e53ccbd0da80
Olaf <olaf.chuszno@gmail.com>
2023-12-28 21:38:26 +0300

    Update back-arrow component

--- resources/views/components/style/back-arrow.blade.php
+++ resources/views/components/style/back-arrow.blade.php
@@ -1,2 +1,27 @@
-@props(['size' => '16px'])
-<svg fill="#ff6200" height="{{ $size }}" width="{{ $size }}" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 490 490" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <polygon points="332.668,490 82.631,244.996 332.668,0 407.369,76.493 235.402,244.996 407.369,413.507 "></polygon> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </g> </g></svg>
+@props(['size' => '16px', 'transform' => ''])
+<svg fill="#ff6200" transform="{{ $transform }}" height="{{ $size }}" width="{{ $size }}" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 490 490" xml:space="preserve">
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+        <g>
+            <g>
+                <polygon points="332.668,490 82.631,244.996 332.668,0 407.369,76.493 235.402,244.996 407.369,413.507 "></polygon>
+            </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+            <g> </g>
+        </g>
+    </g>
+</svg>
