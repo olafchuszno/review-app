bd2702dca6f80eb8122be98cc5183d3661ea0514
Olaf <olaf.chuszno@gmail.com>
2023-12-28 21:17:25 +0300

    Update style

--- resources/views/topic/index.blade.php
+++ resources/views/topic/index.blade.php
@@ -1,19 +1,19 @@
 <x-layout>
     <x-main-airport :airport="$airport">
 
-        <h1 class="text-primaryIndigo text-2xl font-bold mb-12 px-4 py-2 rounded-2xl bg-stone-300">
+        <h1 class="text-primaryIndigo text-2xl font-bold mb-12 px-5 py-2 rounded-2xl bg-raisinBlack">
             What information are you looking for?
         </h1>
 
         <x-topic-grid>
 
             @foreach ($topics as $topic)
-                <x-topic-link href="/airports/{{ $airport->code }}/topics/{{ $topic->name }}">
-                    {{ $topic->name }}
-                </x-topic-link>
+            <x-topic-link href="/airports/{{ $airport->code }}/topics/{{ $topic->name }}">
+                {{ $topic->name }}
+            </x-topic-link>
             @endforeach
 
         </x-topic-grid>
-        
+
     </x-main-airport>
 </x-layout>
