30ebee18205cac96773caa32fd41eb8713ed2b4d
Olaf <olaf.chuszno@gmail.com>
2023-12-28 21:39:07 +0300

    Update short-grid component (visibility, link and colors)

--- resources/views/components/contribution/short-grid.blade.php
+++ resources/views/components/contribution/short-grid.blade.php
@@ -4,19 +4,24 @@
 
     @foreach ($contributions as $contribution)
 
-    <div class="col-span-3 border rounded-2xl border-neutralStone bg-indigo-300">
+    <div class="col-span-3 border rounded-2xl border-neutralStone bg-primaryIndigo">
 
-        <table class="text-primaryIndigo mt-2">
+        <table class="text-raisinBlack mt-2">
 
-            <tr class="p-2 gap-4 font-bold lg:grid lg:grid-cols-3">
-                <td class="col-span-1 lg:text-lg p-2 sm:text-md text-center">
+            <tr class="p-2 gap-4 font-bold lg:grid lg:grid-cols-8 flex">
+                <td class="col-span-2 lg:text-lg p-2 sm:text-md text-center">
                     <a href="contributions/{{ $contribution->id }}">
                         {{ $contribution->answers->first()->question->body }}
                     </a>
                 </td>
-                <td class="col-span-2 pb-1 sm:text-md overflow-hidden">
+                <td class="col-span-5 pb-1 flex items-center sm:text-md overflow-hidden">
                     <a href="contributions/{{ $contribution->id }}" class="break-words">
                         {{ $contribution->answers->first()->body }}
+                    </a>
+                </td>
+                <td class="flex items-center">
+                    <a href="contributions/{{ $contribution->id }}" class="p-3 flex min-w-full justify-center">
+                        <x-style.back-arrow transform="rotate(180)"></x-style.back-arrow>
                     </a>
                 </td>
             </tr>
