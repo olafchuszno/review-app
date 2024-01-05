c91c3275e280992d16ec07377f871f44fe9875a6
Olaf <olaf.chuszno@gmail.com>
2024-01-05 15:15:47 +0300

    Write like_or_unlike_a_contribution method

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Contribution;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class Like extends Model
@@ -20,5 +21,30 @@ class Like extends Model
     public function likable()
     {
         return $this->morphTo();
+    }
+
+    static function like_or_unlike_a_contribution(Contribution $contribution): bool
+    {
+        $like = Like::where([
+            'likable_id' => $contribution->id,
+            'likable_type' => 'App\Models\Contribution'
+        ]);
+
+        // Check whether hasn't user already liked the contribution
+        if ($like->count()) {
+
+            $like->delete();
+
+            return false;
+        }
+
+        // Like the contribution
+        Like::factory()->create([
+            'user_id' => auth()->id(),
+            'likable_type' => 'App\Models\Contribution',
+            'likable_id' => $contribution->id
+        ]);
+
+        return true;
     }
 }
